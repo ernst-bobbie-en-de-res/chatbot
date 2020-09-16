03ab9191ed10f85ac1d30cf33bdf9e8efe843160
jurruh <jurrevriesen@gmail.com>
2020-09-16 10:57:18 +0200

    hackity hack

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    var d1 = document.getElementById('root');
+    d1.insertAdjacentHTML('beforebegin', `    <a-scene vr-mode-ui="enabled: false" embedded arjs="sourceType: webcam; debugUIEnabled: false;sourceWidth:360; sourceHeight:120; displayWidth: 360; displayHeight: 120;"> 
+      <a-entity scale="0.25 0.25 0.25" gltf-model="/untitled.glb" modify-materials look-at="[gps-camera]"
+      gps-entity-place="latitude: 51.93354636; longitude: 6.65203426;"></a-entity>
+  
+      <a-camera gps-camera rotation-reader></a-camera>
+    </a-scene>`);
+
+  }, []);
+
   return (
+
     <div className="App">
 
 
