5608be3b45e414e9a31f9c1260251b927433e81b
jurruh <jurrevriesen@gmail.com>
2020-09-16 10:33:08 +0200

    6320

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ function App() {
   return (
     <div className="App">
 
-      <a-scene vr-mode-ui="enabled: false" embedded arjs="sourceType: webcam; debugUIEnabled: false;sourceWidth:360; sourceHeight:120; displayWidth: 360; displayHeight: 120;">
-
-        <a-entity scale="0.25 0.25 0.25" position="0 -1 0" gltf-model="untitled.glb" modify-materials look-at="[gps-camera]"
-          gps-entity-place="latitude: 51.98753116; longitude: 5.95172667;"></a-entity>
-
-        <a-camera gps-camera rotation-reader></a-camera>
-      </a-scene>
 
     </div>
   );
