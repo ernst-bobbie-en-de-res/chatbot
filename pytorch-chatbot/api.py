e98bf4c136503a97937e62388bf15b18d18a2fdf
Matthieu Kuivenhoven <matthieukuivenhoven@gmail.com>
2020-10-28 15:36:09 +0100

    Tussentijdse commit
    posten werkt niet mee met json.dump. Morgen weer mee verder

--- pytorch-chatbot/api.py
+++ pytorch-chatbot/api.py
@@ -2,7 +2,7 @@ import flask
 from flask import request, jsonify
 from flask_cors import CORS
 from chat import respond
-from nodeService import getNodesByIds
+from nodeService import getNodesByIds, addNode
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
@@ -19,16 +19,21 @@ def message():
     response = respond(message)
     return jsonify(response)
 
-@app.route('/api/v1/nodes', methods=['GET'])
-def getNode():
-    if 'nodeId' in request.args:
-        nodeId = int(request.args['nodeId'])
-        response = getNodesByIds([nodeId])
-    elif 'nodeIds' in request.args:
-        castIdsArray = [int(numeric_string) for numeric_string in request.args.getlist('nodeIds')]
-        response = getNodesByIds(castIdsArray)
-    else:
-        return "Please specify a nodeId!"
-    return jsonify(response)
+@app.route('/api/v1/nodes', methods=['GET', 'POST'])
+def node():
+    if request.method == 'GET':
+        if 'nodeId' in request.args:
+            nodeId = int(request.args['nodeId'])
+            response = getNodesByIds([nodeId])
+        elif 'nodeIds' in request.args:
+            castIdsArray = [int(numeric_string) for numeric_string in request.args.getlist('nodeIds')]
+            response = getNodesByIds(castIdsArray)
+        else:
+            return "Please specify a nodeId!"
+        return jsonify(response)
+    elif request.method == 'POST':
+        body = request.get_json()
+        addNode(body)
+        return jsonify(body)
 
 app.run(host='0.0.0.0')
--- pytorch-chatbot/nodeService.py
+++ pytorch-chatbot/nodeService.py
@@ -8,3 +8,12 @@ def getNodesByIds(nodeIds):
             if node['id'] in nodeIds:
                 matchedNodes.append(node)
         return matchedNodes
+
+def getAllNodes():
+    with open('nodes.json', 'r') as f:
+        nodes = json.load(f)
+        return nodes['nodes']
+
+def addNode(node):
+    with open('nodes.json', 'w') as f:
+        json.dump(node, f)
