58a3305bc34dc7345c84ad26184258e66ad783c4
Matthieu Kuivenhoven <matthieukuivenhoven@gmail.com>
2020-10-29 10:38:07 +0100

    Only get and set nodes

--- pytorch-chatbot/api.py
+++ pytorch-chatbot/api.py
@@ -2,7 +2,7 @@ import flask
 from flask import request, jsonify
 from flask_cors import CORS
 from chat import respond
-from nodeService import getNodesByIds, addNode
+from nodeService import getNodes, setNodes
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
@@ -22,18 +22,11 @@ def message():
 @app.route('/api/v1/nodes', methods=['GET', 'POST'])
 def node():
     if request.method == 'GET':
-        if 'nodeId' in request.args:
-            nodeId = int(request.args['nodeId'])
-            response = getNodesByIds([nodeId])
-        elif 'nodeIds' in request.args:
-            castIdsArray = [int(numeric_string) for numeric_string in request.args.getlist('nodeIds')]
-            response = getNodesByIds(castIdsArray)
-        else:
-            return "Please specify a nodeId!"
+        response = getNodes()
         return jsonify(response)
     elif request.method == 'POST':
         body = request.get_json()
-        addNode(body)
-        return jsonify(getAllNodes())
+        setNodes(body)
+        return jsonify(body)
 
 app.run(host='0.0.0.0')
--- pytorch-chatbot/nodeService.py
+++ pytorch-chatbot/nodeService.py
@@ -1,22 +1,9 @@
 import json
 
-def getNodesByIds(nodeIds):
+def getNodes():
     with open('nodes.json', 'r') as f:
-        nodes = json.load(f)
-        matchedNodes = []
-        for node in nodes['nodes']:
-            if node['id'] in nodeIds:
-                matchedNodes.append(node)
-        return matchedNodes
+        return json.load(f)
 
-def getAllNodes():
-    with open('nodes.json', 'r') as f:
-        nodes = json.load(f)
-        return nodes['nodes']
-
-def addNode(node):
-    currentNodes = getAllNodes()
-    currentNodes.append(node)
-    jsonObj = json.loads(currentNodes)
+def setNodes(nodes):
     with open('nodes.json', 'w') as f:
-        json.dump(jsonObj, f)
+        json.dump(nodes, f)
