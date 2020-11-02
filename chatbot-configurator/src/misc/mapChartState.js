62f4d09e3535e32e5a3d13e0c4ddcf0d72bf58b5
jurruh <jurrevriesen@gmail.com>
2020-11-02 11:48:20 +0100

    Even cleaner

--- chatbot-configurator/src/misc/mapChartState.js
+++ chatbot-configurator/src/misc/mapChartState.js
@@ -1,16 +1,10 @@
-export const mapChartState = (chartState) => {
+export const mapChartState = (chartState) => Object.values(chartState.nodes).map((node) => ({
+    id: node.id,
+    text: node.properties.custom,
+    subNodes: Object.values(chartState.links)
+        .filter(link => link.from.nodeId === node.id)
+        .map(link => ({
+            to: link.to.nodeId
+        }))
+}));
 
-
-    console.log(Object.values(chartState.nodes))
-
-    return Object.values(chartState.nodes).map((node) => ({
-        id: node.id,
-        text: node.properties.custom,
-        subNodes: Object.values(chartState.links)
-            .filter(link => link.from.nodeId === node.id)
-            .map(link => ({
-                to: link.to.nodeId
-            }))
-    }));
-
-}
