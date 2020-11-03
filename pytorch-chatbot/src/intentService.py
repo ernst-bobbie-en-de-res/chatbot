f8fa07349e629b66afae36e1b0ea84f707e427d4
Matthieu Kuivenhoven <matthieukuivenhoven@gmail.com>
2020-11-03 11:44:09 +0100

    Delete intentService.py

--- pytorch-chatbot/src/intentService.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def getIntents():
-    with open('intents.json', 'r') as f:
-        return json.load(f)
-
-def setIntents(intents):
-    with open('intents.json', 'w') as f:
-        json.dump(intents, f)
